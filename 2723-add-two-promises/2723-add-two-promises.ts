98e3c49563098eebe02025ca55c638d53ee9798a
Eric Lloyd Ochieng <31162324+lloydagola@users.noreply.github.com>
2024-06-27 22:06:58 +0300

    Time: 67 ms (64.09%), Space: 51.6 MB (63.95%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.ts
@@ -0,0 +1,13 @@
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+    
+    const [val1, val2] = await Promise.all([promise1, promise2]);
+    
+    return val1 + val2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
