682cc4bdd57d0e73512fb2e27cfd59716de8ab0e
Eric Lloyd Ochieng <31162324+lloydagola@users.noreply.github.com>
2024-07-06 20:21:04 +0300

    Time: 63 ms (24.06%), Space: 50.4 MB (14.27%) - LeetHub

---
+++ 2621-sleep/2621-sleep.ts
@@ -0,0 +1,16 @@
+async function sleep(millis: number): Promise<void> {
+    
+    
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(millis)
+        }, millis)
+    })
+    
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
