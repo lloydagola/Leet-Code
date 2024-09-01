624be2591ce7bae6eb29e8eac1071bb848d4b26b
Eric Lloyd Ochieng <31162324+lloydagola@users.noreply.github.com>
2024-09-01 12:19:54 +0300

    Time: 57 ms (91.90%), Space: 51.8 MB (24.31%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.ts
+++ 2637-promise-time-limit/2637-promise-time-limit.ts
@@ -5,7 +5,13 @@ function timeLimit(fn: Fn, t: number): Fn {
     return async function(...args) {
         return new Promise((resolve, reject) => {
             const timeout = setTimeout(()=> reject("Time Limit Exceeded"), t);            
-            fn(...args).then(result => resolve(result)).catch(reject);      
+            fn(...args).then(result => {
+                clearTimeout(timeout)
+                resolve(result)
+            }).catch(error => {
+                clearTimeout(timeout);
+                reject(error)
+            });      
         });
     }
 };
